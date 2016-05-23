8ed0bc6058048e0447f063e666afdec3b99c44c6
Lenovo <Lenovo@180.149.29.49>
2016-05-23 23:18:14 +0600

    New

---
+++ src/logic2/hell.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic2;
+
+/**
+ *
+ * @author nazrul
+ */
+public class hell {
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
